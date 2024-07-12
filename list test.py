a530b5d0d44140b1301249c698756ab020311f99
Zartheren <czartheking6225@yahoo.com>
2024-07-11 21:59:02 -0500

    Got the list test working

--- list test.py
+++ list test.py
@@ -1,18 +1,20 @@
+import collections
+import numpy as np
+import re
 
-deck = []
+deckList = collections.deque([])
 
+# Goes through given deck list and separates the card name from the card count
 while True:
     addCard = input()
+    # Looks for a return as the end of the deck list
     if addCard == '':
         break
+    # Looks at given card name and removes the " x" in each line and separates the name and count by regular expression
     else:
-        deck.append(addCard)
-
-deck = ' x'.join(deck).split(' x')
+        regexResults = re.search(r'(.*) x([0-9]*)', addCard)
+        deckList.append(regexResults.groups())
 
 print("Your deck list is:")
 
-# for addCard in deck:
-#    print(addCard)
-
-print(deck)
+print(deckList)
