13e0f526f6b88a94904610c3880e9dc59627ea44
Zartheren <czartheking6225@yahoo.com>
2024-07-18 17:08:57 -0500

    Got deck filling with all iterations of all cards

--- list test.py
+++ list test.py
@@ -6,6 +6,7 @@ import numpy as np
 import re
 import random
 
+# Deck list that will hold the exported info from EDO Pro
 deckList = collections.deque([])
 
 # Goes through given deck list and separates the card name from the card count
@@ -19,33 +20,41 @@ while True:
         regexResults = re.search(r'(.*) x([0-9]*)', addCard)
         deckList.append(regexResults.groups())
 
-# # Output what you entered, in the stored format
-# print("Your deck list is:")
-# print(deckList)
-#
-# print('Number of card names:')
-# print(len(deckList))
-
 # Making list of only card names
 cardNames = [x[0] for x in deckList]
+# Value for incrementing the number in the following list output
 numList = 0
+# Making value for indexing what cards have been added to the deck to be drawn
+filling = 0
+# Making list of what is going to be complete deck list that will be used for drawing from
+deck = collections.deque([])
+
+# Filling deck with cards by their number count
+for card in deckList:
+    while filling < int(card[1]):
+        deck.append(card[0])
+        filling += 1
+    filling = 0
 
 print("Which of these single cards would you need one of in a opening hand to make a play")
 print("Type the number of the corresponding card and hit enter after each one")
 print("Enter an empty entry after you have finished")
 
+# Printing list of cards with number in front so that the user can return what cards are needed in first hand
 while True:
     if len(cardNames) > numList:
         print(f"{(numList + 1)}. {cardNames[numList]}")
 
-        numList = numList + 1
+        numList += 1
 
     else:
         print("________________________________________________________________________________")
         break
 
+# Making list for the needed cards in first hand
 neededCards = collections.deque([])
 
+# collecting numbers from user of needed cards and referencing them back to corresponding number in list
 while True:
     addCard = input()
     if addCard == "":
@@ -57,15 +66,6 @@ while True:
     else:
         print("Please enter a number corresponding to one of the card numbers above")
 
-# # Print out list of cards that user marked as needed
-# print(neededCards)
+random.shuffle(deck)
 
-# Filling deck with cards by their number count
-filling = 0
-deck = collections.deque([])
-
-while filling <= len(cardNames):
-    deck.append()
-
-random.shuffle(deckList)
-
+print(deck)
